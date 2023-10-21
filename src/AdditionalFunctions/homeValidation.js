f21df7bfad6f561bd849ab6040d71612e2a00139
Rakul06 <114053091+Rakul06@users.noreply.github.com>
2023-10-21 14:09:16 +0530

    Add files via upload

---
+++ src/AdditionalFunctions/homeValidation.js
@@ -0,0 +1,52 @@
+export const homeValidation=(carBookingHome,setHomeErrMssg)=>{
+
+    let selectTF=false,locationTF=false,pickupTF=false,dropTF=false;
+
+    if(carBookingHome.login===true){
+        if(carBookingHome.carType===""){
+            setHomeErrMssg((obj)=> { return {...obj,select:"please select a car type"}})
+        }
+        else{
+            setHomeErrMssg((obj)=> { return {...obj,select:""}})
+            selectTF=true;
+        }
+        if(carBookingHome.location===""){
+            setHomeErrMssg((obj)=> { return {...obj,location:"please enter your location"}})
+        }
+        else{
+            setHomeErrMssg((obj)=> { return {...obj,location:""}})
+            locationTF=true;
+        }
+        if(carBookingHome.pickup===""){
+            setHomeErrMssg((obj)=> { return {...obj,pickup:"please enter pickup date"}})
+        }
+        else{
+            setHomeErrMssg((obj)=> { return {...obj,pickup:""}})
+            pickupTF=true;
+        }
+        if(carBookingHome.drop===""){
+            setHomeErrMssg((obj)=> { return {...obj,drop:"please enter your location"}})
+        }
+        else{
+            setHomeErrMssg((obj)=> { return {...obj,drop:""}})
+            dropTF=true;
+        }
+
+        if(selectTF && locationTF && pickupTF && dropTF){
+            setHomeErrMssg((obj)=>{
+               return{
+                    ...obj,
+                    select:"",
+                    location:"",
+                    pickup:"",
+                    drop:""
+                }
+            })
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
